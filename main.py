3e50882b8d7cacdeae8e3dc92009d6538e95608f
BIGGUY29 <86998249+BIGGUY29@users.noreply.github.com>
2021-07-09 11:17:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+import os
+from discord.ext import commands
+import datetime
+from tools.alive import keep_alive
+from mod.Ban import *
+
+bot = commands.Bot(command_prefix='b!')
+bot.remove_command('help')
+
+color = 0x45608f
+
+@bot.event
+async def on_ready():
+    print("Started")
+
+bot.run('ODYyOTMyOTE5MDM4NzcxMjAx.YOfi6A.KjIGQr-KudjJfGJWtpHLg5rx0CQ')
